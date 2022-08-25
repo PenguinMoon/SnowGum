f7a1170854f6273916489abebc7f2a995cb35faa
Luna <36724380+PenguinMoon@users.noreply.github.com>
2022-08-25 22:08:53 +1000

    First project commit

---
+++ Source/SnowGum/SnowGum.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SnowGum.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnowGum, "SnowGum" );
---
+++ Source/SnowGum/SnowGumGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SnowGumGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNOWGUM_API ASnowGumGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
